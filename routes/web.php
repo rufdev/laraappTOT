390108cf4988c5c53ba96e8e6af9c1fccbecf503
Rufino John <aguilarufino@gmail.com>
2024-09-17 10:18:28 +0800

    added page 1 - 3

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,17 @@ Route::get('/dashboard', function () {
     return view('pages.dashboard');
 });
 
+Route::get('/page1', function () {
+    return view('pages.page1');
+});
+
+Route::get('/page2', function () {
+    return view('pages.page2');
+});
+
+Route::get('/page3', function () {
+    return view('pages.page3');
+});
+
 
 
